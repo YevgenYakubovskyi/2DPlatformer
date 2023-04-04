9c3dd09941dc621d35a07509e8c49ca7bc941db2
izauma <akuma.izao@gmail.com>
2023-04-04 09:53:47 +0300

    UI controller

---
+++ Assets/Scripts/Core/GameLevelInitializer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Player;
+using UnityEngine;
+
+namespace Core
+{
+    public class GameLevelInitializer : MonoBehaviour
+    {
+        [SerializeField] private PlayerEntity _playerEntity;
+        [SerializeField] private GameUIInputView _gameUIInputView;
+
+        private ExternalDevicesInputReader _externalDevicesInput;
+        private PlayerBrain _playerBrain;
+
+        private bool _onPause = false;
+        
+        
+        private void Awake()
+        {
+            _externalDevicesInput = new ExternalDevicesInputReader();
+            _playerBrain = new PlayerBrain(_playerEntity, new List<IEntityInputSource>
+            {
+                _gameUIInputView,
+                _externalDevicesInput
+            });
+        }
+
+        private void Update()
+        {
+            if (_onPause)
+                return;
+            
+            _externalDevicesInput.OnUpdate();
+        }
+
+        private void FixedUpdate()
+        {
+            if (_onPause)
+                return;
+            
+            _playerBrain.OnFixedUpdate();
+        }
+    }
+}
