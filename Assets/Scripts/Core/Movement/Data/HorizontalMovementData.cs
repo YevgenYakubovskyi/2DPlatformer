e04e593e8df1c04d8f0e0211db79ba90c35fcaad
Izauma <114006465+YevgenYakubovskyi@users.noreply.github.com>
2023-04-10 21:35:53 +0300

    Revert "Revert "Players stats""

---
+++ Assets/Scripts/Core/Movement/Data/HorizontalMovementData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Core.Enums;
+using UnityEngine;
+
+namespace Core.Movement.Data
+{
+    [Serializable]
+    public class HorizontalMovementData
+    {
+        [field: SerializeField] public Direction Direction { get; private set; }
+        
+    }
+}
